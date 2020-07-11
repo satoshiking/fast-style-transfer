4c8e185bcd2f1dca348df0b3ae3a4412f6daffe2
satoshiking <RGBglobal84$>
2020-07-11 14:53:42 +0300

    starting data

--- evaluate.sh
+++ evaluate.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-python fast-style-transfer/evaluate.py --checkpoint fast-style-transfer/models/scream.ckpt --in-path fast-style-transfer/input/ --out-path fast-style-transfer/output/ --allow-different-dimensions
+python fast-style-transfer-myfork/evaluate.py --checkpoint fast-style-transfer-myfork/models/scream.ckpt --in-path fast-style-transfer-myfork/input/ --out-path fast-style-transfer-myfork/output/ --allow-different-dimensions
